b3e6fa6bc72126f4864a965de1235dc49f126ffd
Saman Kumara <samankumararajapaksha660@gmail.com>
2023-08-01 17:03:52 +0530

    modified

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
+var i = 0;
+var txt = 'Lorem ipsum typing effect!'; 
+var speed = 50; 
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("sanana").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
